826eef89143530fec57bf1e227c72d4e242a682b
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-02-05 14:22:31 -0500

    Show hidden files in telescope pickers

--- nvim/init.lua
+++ nvim/init.lua
@@ -294,9 +294,17 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      theme = "dropdown",
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+  },
   extensions = {
     file_browser = {
       hijack_netrw = true,
+      hidden = true
     },
     workspaces = {
       -- keep insert mode after selection in the picker, default is false
