ed641a4066deecceb5b055249eaebc7a4e78d624
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-02-05 14:23:07 -0500

    Update toggleterm.lua

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,16 +1,20 @@
 return {
     'akinsho/toggleterm.nvim',
-    version = "*", 
+    version = "*",
     config = function()
         require('toggleterm').setup({
             open_mapping = '<leader>tt',
+            on_open = function(term)
+                vim.cmd("startinsert!")
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+            end,
             insert_mappings = true,
             terminal_mappings = true,
             close_on_exit = true,
             auto_scroll = true,
             shade_terminals = false,
-            shading_factor = 0,
-            direction = 'float', -- 'vertical', 'horizontal', 'tab', 'float'
+            shading_factor = false,
+            direction = 'float',   -- 'vertical', 'horizontal', 'tab', 'float'
             float_opts = {
                 border = 'curved', -- 'single', 'double', 'shadow', 'curved'
                 winblend = 0,
