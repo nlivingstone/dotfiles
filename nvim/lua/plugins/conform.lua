4dfcc35c09bdb4a94b9cb2ba7a6b0f76abf63e6b
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-02-06 18:42:39 -0500

    Use prettierd to format css

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,7 +8,26 @@ return {
     conform.setup({
       formatters_by_ft = {
         vue = { "rustywind" },
-        php = { "rustywind", "phpcsfixer", "blade-formatter" }
+        php = { "rustywind", "phpcsfixer", "blade-formatter" },
+        css = { "prettierd" },
+      },
+      ft_parsers = {
+            javascript = "babel",
+            javascriptreact = "babel",
+            typescript = "typescript",
+            typescriptreact = "typescript",
+            vue = "vue",
+            css = "css",
+            scss = "scss",
+            less = "less",
+            html = "html",
+            json = "json",
+            jsonc = "json",
+            yaml = "yaml",
+            markdown = "markdown",
+            ["markdown.mdx"] = "mdx",
+            graphql = "graphql",
+            handlebars = "glimmer",
       },
       format_on_save = {
         lsp_fallback = true,
