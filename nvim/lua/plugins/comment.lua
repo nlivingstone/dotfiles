e0ddc361a3884fdd7ffb77a807c5bd0514fa3463
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-02-06 18:41:52 -0500

    Add commenting key bindings

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,19 @@
 return {
 	'numToStr/Comment.nvim',
 	opts = {
-		-- add any options here
+		toggler = {
+			---Line-comment toggle keymap
+			line = 'gcc',
+			---Block-comment toggle keymap
+			block = 'gbc',
+		},
+		---LHS of operator-pending mappings in NORMAL and VISUAL mode
+		opleader = {
+			---Line-comment keymap
+			line = 'gc',
+			---Block-comment keymap
+			block = 'gb',
+		},
 	},
 	lazy = false,
 }
