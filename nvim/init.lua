396d3068dd6e03b027ada11482fbaa28bf2c91a4
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-02-09 17:32:51 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -350,7 +350,7 @@ vim.keymap.set('n', '<D-f>', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<D-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set({ 'n', 'i', 'v' }, '<D-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
